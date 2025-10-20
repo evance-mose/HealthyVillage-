2860352111d90042267832b5501b8005153308d0
evance-mose <evasmose@gmail.com>
2025-10-20 11:46:04 +0200

    Refactor HouseHoldDetails.vue to improve household data display, changing to location name and description, and update component structure for better readability.

--- src/views/HouseHoldDetails.vue
+++ src/views/HouseHoldDetails.vue
@@ -2,53 +2,48 @@
   <IonPage>
     <IonHeader>
       <IonToolbar>
-        <ion-back-button
+        <IonBackButton
           default-href="/"
           :icon="arrowBackOutline"
           class="back-arrow"
           slot="start"
           text=""
-        >
-        </ion-back-button>
-        <IonTitle>HouseHold Details</IonTitle>
+        />
+        <IonTitle>Household Details</IonTitle>
       </IonToolbar>
     </IonHeader>
+
     <IonContent class="ion-padding">
-      <h1>{{ household?.name }}</h1>
-      <h4>{{ household?.description }}</h4>
+      <h1>{{ household?.location?.name }}</h1>
+      <h4>{{ household?.location?.description }}</h4>
     </IonContent>
   </IonPage>
 </template>
+
 <script lang="ts" setup>
 import { getHouseholdByID } from "@/utils/functions";
-import { HouseHold } from "@/utils/types";
 import {
   IonContent,
   IonHeader,
   IonPage,
   IonTitle,
   IonToolbar,
-  IonFab,
-  IonFabButton,
-  IonIcon,
-  IonModal,
-  IonItem,
-  IonButton,
   IonBackButton,
 } from "@ionic/vue";
 import { arrowBackOutline } from "ionicons/icons";
 import { onMounted, ref } from "vue";
 import { useRoute } from "vue-router";
-const router = useRoute();
-const id = router.params.id;
-const household = ref();
 
-onMounted(() => {
-  console.log(getHouseholdByID(id));
+const route = useRoute();
+const id = route.params.id as string;
+const household = ref<any>(null);
+
+onMounted(async () => {
+  household.value = await getHouseholdByID(id);
 });
 </script>
 
-<style setup>
+<style scoped>
 .back-arrow {
   color: white;
 }
